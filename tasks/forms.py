13439ad9faa4bd6ac189eb13f4326b20185b8f50
Laiba Saleem <laibasaleem068@gmail.com>
2025-12-03 21:13:00 +0500

    chore: Update form

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,9 +5,22 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class TaskForm(forms.ModelForm):
-    assignee = forms.ModelMultipleChoiceField(queryset=User.objects.all(), required=False)
-    due_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type':'datetime-local'}))
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+    )
+
+    due_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'})
+    )
 
     class Meta:
         model = Task
-        fields = ['title','description','due_date','status','assignee']
+        fields = ['title', 'description', 'due_date', 'status', 'assignees']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }

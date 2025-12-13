b6f3a527de90464f2643c961be65b7e7b621ac87
Laiba Saleem <laibasaleem068@gmail.com>
2025-12-13 15:59:44 +0500

    chore: Update DB features

--- project/settings.py
+++ project/settings.py
@@ -50,13 +50,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 ASGI_APPLICATION = "project.asgi.application"
 
 
-# Database (Postgres recommended)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
+
+
 
 
 # For quick dev use sqlite, uncomment:
